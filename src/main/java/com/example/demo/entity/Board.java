f3fb5fc3fe6420c6e6022d5241e0ad403e96809f
yy-ham <yyham.dev@gmail.com>
2023-08-02 16:31:52 +0900

    Feat(entity): Create Board

---
+++ src/main/java/com/example/demo/entity/Board.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "board")
+public class Board {
+	@Id
+	private int board_no;
+	
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private int hit;
+}
