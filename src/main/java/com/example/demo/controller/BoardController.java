7c53632fdbbcde2727db1d00910f718e5e225dd0
yy-ham <yyham.dev@gmail.com>
2023-08-02 16:46:13 +0900

    Feat(controller): Update BoardController

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -1,12 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.dao.BoardDAO;
 
 @Controller
 public class BoardController {
+	@Autowired
+	private BoardDAO boardDAO;
+	
 	@GetMapping("/board/list")
-	public String list() {
+	public String list(Model model) {
+		model.addAttribute("list", boardDAO.findAll());
 		return "/board/list";
 	}
 }
