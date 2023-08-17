9ce4748d57934c65c4c50f5d47b6214dea0e21ca
yy-ham <yyham.dev@gmail.com>
2023-08-17 15:38:28 +0900

    feat: Set querydsl configuration

--- src/main/java/com/example/demo/entity/Board.java
+++ src/main/java/com/example/demo/entity/Board.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
